fe7b178617c72529e823feb129ab32ea336bac12
Paulo Jonas <pjonas21@gmail.com>
2023-07-04 14:41:49 -0300

    fix bug mongodb

--- aula_sqlalchemy/integrationWithMongo/integration_mongo.py
+++ aula_sqlalchemy/integrationWithMongo/integration_mongo.py
@@ -4,7 +4,7 @@ import pprint
 import pymongo as pyM
 
 cliente = pyM.MongoClient(
-    "mongodb+srv://pjonas21:pjas@clusterlearrn.z1cmxtg.mongodb.net/?retryWrites=true&w=majority")
+    "mongodb+srv://<username>:<password>@clusterlearrn.z1cmxtg.mongodb.net/?retryWrites=true&w=majority")
 
 db = cliente.test
 collection = db.test_collection
